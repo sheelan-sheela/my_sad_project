1f3e53f111c7ff2eee0889809416979864269e58
sheelan hassan <80004740+sheelan236@users.noreply.github.com>
2022-05-21 18:37:46 +0300

    hi

--- pages/forget_pass.tsx
+++ pages/forget_pass.tsx
@@ -24,7 +24,7 @@ import {
 
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
-const Links = ["Dashboard", "Projects", "Team"];
+const Links = ["home", "classes"];
 
 const NavLink = ({ children }: { children: ReactNode }) => (
   <Link
@@ -76,18 +76,12 @@ export default function Simple() {
                 cursor={"pointer"}
                 minW={0}
               >
-                <Avatar
-                  size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
-                />
+                <Avatar size={"sm"} src={""} />
               </MenuButton>
               <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
+                <MenuItem>4th sw</MenuItem>
+                <MenuItem>4th ai</MenuItem>
                 <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
               </MenuList>
             </Menu>
           </Flex>
